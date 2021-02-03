5252a2399d34e3d4974332bd1df1fd5e47b69ba8
AnJaWr <october3@o2.pl>
2021-02-03 17:36:15 +0100

    constelation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,5 +81,16 @@ $(document).ready(function () {
   $('.points-mask').hover(function () {
     $('.circle').toggleClass('circle--hover');
   });
+  AddAnimationClass();
 });
-
+//Create a var to store the index of red element
+var count = -1;
+function AddAnimationClass() {
+  var boxes = $('.line');
+  var boxLength = boxes.length - 1;
+  //Check if the actual item isn't more than the length then add 1 otherway restart to 0
+  count < boxLength ? count++ : (count = 0);
+  //Remove the class and add it to the new target
+  boxes.removeClass('animation').eq(count).addClass('animation');
+}
+setInterval(AddAnimationClass, 13000);
