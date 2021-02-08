fc15db65e73bae743674cd7f2054b41dffd4391e
AnJaWr <october3@o2.pl>
2021-02-08 20:21:12 +0100

    toggle improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  // $('.section').hide(); //hide
   $('.blockquote').show();
 
   $('.navigation-button').click(function () {
@@ -12,14 +11,11 @@ $(document).ready(function () {
   });
   AddAnimationClass();
 });
-//Create a var to store the index of red element
 var count = -1;
 function AddAnimationClass() {
   var boxes = $('.line');
   var boxLength = boxes.length - 1;
-  //Check if the actual item isn't more than the length then add 1 otherway restart to 0
   count < boxLength ? count++ : (count = 0);
-  //Remove the class and add it to the new target
   boxes.removeClass('animation').eq(count).addClass('animation');
 }
 setInterval(AddAnimationClass, 13000);
