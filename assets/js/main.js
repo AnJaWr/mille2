35bfa0e04f130c37b58e8f12d0df5f24559d1646
AnJaWr <october3@o2.pl>
2021-02-11 22:29:04 +0100

    new version

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,5 @@ $(document).ready(function () {
   $('.navigation-button').click(function () {
     $('.section').hide(); //hide
     $('.' + $(this).data('id')).fadeIn(1000);
-
-    if ($('.blog').is(':visible')) {
-      $('#heading').fadeTo(300, 0);
-    } else {
-      $('#heading').fadeTo(500, 1);
-    }
   });
-
-  $('.points-mask').hover(function () {
-    $('.circle').toggleClass('circle--hover');
-  });
-  AddAnimationClass();
 });
-var count = -1;
-function AddAnimationClass() {
-  var boxes = $('.line');
-  var boxLength = boxes.length - 1;
-  count < boxLength ? count++ : (count = 0);
-  boxes.removeClass('animation').eq(count).addClass('animation');
-}
-setInterval(AddAnimationClass, 13000);
